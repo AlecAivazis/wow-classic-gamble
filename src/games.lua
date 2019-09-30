9f5da61525415ab548ff574ef1e3bd3e8ca2c58a
Alec Aivazis <alec@aivazis.com>
2019-09-30 03:39:05 -0700

    better messaging

--- src/games.lua
+++ src/games.lua
@@ -6,7 +6,8 @@ Games["HiLo"] ={
     Explain = "Some really helpful text",
     Execute = function (players, maxRisk) 
         -- notify people that the game has actually started
-        GambleCore:Say("Okay! Let's begin. Everyone roll a number between 0  and " .. maxRisk .. ".")
+        GambleCore:Say("Okay! Let's begin. Everyone roll a number between 1  and " .. maxRisk .. ".")
+        GambleCore:Say("In case you didn't know, you can do that with /roll " .. maxRisk)
 
         -- we have to start by collecting the rolls from every player
         GambleCore:CollectSameRoll(players, 1, maxRisk, function(results) 
